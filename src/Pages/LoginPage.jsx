9ddd882e815381acf4dd25e9001a77de8084fb12
babbiepeach <139907596+babbiepeach@users.noreply.github.com>
2025-03-24 10:11:42 +0100

    Update LoginPage

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -99,7 +99,7 @@ const LoginPage = () => {
                         type='submit'
                         disabled={isLoading}
                         className={`w-[60%] text-white font-medium py-2 rounded-lg transition ${isLoading ? 'bg-gray-600' : 'bg-dark-blue'}`}>
-                        {isLoading? 'Logging In...' : 'Login'}
+                        {isLoading ? 'Logging In...' : 'Login'}
                     </button>
                     <p className='pt-3' onClick={() => setShowRegisterForm(true)}>
                         or <span className='underline cursor-pointer'>Register</span>.
@@ -253,7 +253,7 @@ const LoginPage = () => {
                         type='submit'
                         disabled={isLoading}
                         className={`w-[60%] text-white font-medium py-2 rounded-lg transition ${isLoading ? 'bg-gray-600' : 'bg-dark-blue'}`}>
-                        {isLoading? 'Registering...' : 'Register'}
+                        {isLoading ? 'Registering...' : 'Register'}
                     </button>
                     <p className='pt-3' onClick={() => setShowRegisterForm(false)}>
                         or <span className='underline cursor-pointer'>Login</span>.
@@ -266,12 +266,12 @@ const LoginPage = () => {
     return (
         <div className='bg-not-white font-poppins flex justify-center items-center w-[100vw] h-[100vh]'>
             <div className='w-3/4 bg-white rounded-2xl h-[85%] flex gap-12'>
-                <div className='flex bg-gradient-to-r rounded-2xl from-light-blue to-bright-yellow justify-center items-center bg-cover w-1/2'>
+                <div className='flex rounded-2xl bg-light-blue justify-center items-center bg-cover w-1/2'>
                     <div className='w-[270px] h-[240px] rounded-lg backdrop-blur-md bg-white/20 p-6 text-center max-w-md flex flex-col justify-center items-center'>
                         <p className="text-white text-lg font-semibold">
                             Connecting <br /> Teachers, Inspiring <br /> Students with{" "}
                             <span className="text-bright-yellow font-bold">Sc</span>
-                            <span className="text-logo-blue font-bold">Learn.</span>
+                            <span className="text-dark-blue font-bold">Learn.</span>
                         </p>
                         <p className="text-white text-sm mt-2">
                             Transforming Education, One <br /> Click at a Time.
